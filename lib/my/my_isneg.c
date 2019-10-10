b7bd367614284ec38f649a7a864569ff4dcce20d
Baron <arthur.baron@epitech.eu>
2019-10-10 16:01:03 +0200

    10/10|16:00|Correction minors in file my_strcmp.c

--- lib/my/my_isneg.c
+++ lib/my/my_isneg.c
@@ -13,6 +13,5 @@ int my_isneg(int n)
         my_putchar('P');
     else
         my_putchar('N');
-
     return (0);
 }
--- lib/my/my_strcmp.c
+++ lib/my/my_strcmp.c
@@ -14,10 +14,12 @@ int my_strcmp(char const *s1, char const *s2) {
 
     if (s2[0] == '\0')
         return (0);
-    for (int i = 0; i < my_strlen(s1); i++)
+    for (int i = 0; i < my_strlen(s1); i++) {
         value_string_one = value_string_one + s1[i];
-    for (int j = 0; j < my_strlen(s2); j++)
+    }
+    for (int j = 0; j < my_strlen(s2); j++) {
         value_string_two = value_string_two + s2[j];
+    }
     if (value_string_one > value_string_two)
         return (1);
     else if (value_string_one < value_string_two)
