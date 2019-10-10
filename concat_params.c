d0220cb28d1f7cc1766da39b112dd69732c9c68f
Baron <arthur.baron@epitech.eu>
2019-10-10 14:43:10 +0200

    10/10|14:42|Task 02 done

--- concat_params.c
+++ concat_params.c
@@ -5,26 +5,51 @@
 ** Task 02
 */
 
+#include <stdlib.h>
+
 int my_strlen(char const *str);
 int my_putstr(char const *str);
 
-char *concat_params(int argc, char **argv)
+int find_strings_size(int argc, char **argv)
 {
     int i = 0;
     int j = 0;
 
-    while (i != argc) {
-        if ((argv[i] > 64 && argv[i] < 91) || (argv[i] > 97 && argv[i] < 123)) {
-            j = j + my_strlen(argv[i]);
-        }
-                    i++;
-        printf("%d", j);
+    while (i < argc) {
+        for (int a = 0; a < my_strlen(argv[i]); a++)
+            j = j + 1;
+        i++;
     }
+    return (j);
 }
 
-int main(int argc, char **argv)
+char backslash_or_not(int i, int argc, char *dest, int dest_position )
 {
-    //my_putstr(concat_params(argc, argv));
-    concat_params(argc, argv);
-    return (0);
+    if (i != argc)
+        dest[dest_position] = '\n';
+    else
+        dest[dest_position] = '\0';
+}
+
+char *concat_params(int argc, char **argv)
+{
+    int i = 0;
+    int array_position = 0;
+    int dest_position = 0;
+    int count_down = 0;
+    char *dest = malloc(find_strings_size(argc, argv) * sizeof(char));
+
+    while (i != argc) {
+        count_down = 0;
+        while (my_strlen(argv[array_position]) > count_down) {
+            dest[dest_position] = argv[array_position][count_down];
+            count_down++;
+            dest_position++;
+        }
+        i++;
+        backslash_or_not(i, argc, dest, dest_position);
+        array_position++;
+        dest_position++;
+    }
+    return (dest);
 }
